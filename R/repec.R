ed378c4d1d074de0322b56e2aee90887e85d225c
Erik Igelström <eigelstrom@gmail.com>
2020-03-17 21:58:22 +0000

    Extract write_references function

--- R/repec.R
+++ R/repec.R
@@ -92,6 +92,19 @@ get_references <- function(results) {
   results
 }
 
+#' Save references downloaded from RePEc IDEAS to a file
+#' 
+#' Saves references retrieved using \code{\link{get_references}} to a file.
+#' 
+#' @seealso \code{\link{repec_search}} for performing the initial search, and
+#'   \code{\link{get_references}} for retrieving reference data.
+write_references <- function(results, file_name) {
+  results$ris_data %>%
+    stats::na.omit() %>%
+    stringi::stri_join_list(sep = "\n") %>%
+    stringi::stri_write_lines(file_name, sep = "\n")
+}
+
 publication_type <- function(name) {
   switch(
     name,
--- scrape.R
+++ scrape.R
@@ -1,29 +1,15 @@
-library(stringi)
-library(magrittr)
-
 library(repecscraper)
 
-################################################################################
-### Configurable settings
-################################################################################
-
-# Where to save the RIS output
-output_file <- "export.ris"
-
-################################################################################
-?repec_search
 results <- repec_search(
   query = '("mental health"| depression| anxiety| well-being| wellbeing| "quality of life"| "life satisfaction"| "psychological distress") + (income*| "social security"| earning*| salar*| wage*| money| financ*| loan*| debt*| lottery| poverty| "cash transfer"| welfare) + (change*| alter*| shock*| w?n)',
   to_page = 0,
   publication_type = "articles",
   search_in = "abstract"
-) %>%
-  get_references()
+)
+  
+results <- get_references(results)
 
-results$ris_data %>%
-  na.omit() %>%
-  stri_join_list(sep = "\n") %>%
-  stri_write_lines(output_file, sep = "\n")
+write_references(results, "export.ris")
 
 print("Done.")
 
