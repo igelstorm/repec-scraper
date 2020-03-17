7deca6dd34f504d4f147a4b916bcd49161db5c00
Erik Igelström <eigelstrom@gmail.com>
2020-03-17 22:21:17 +0000

    Document and export write_references

--- R/repec.R
+++ R/repec.R
@@ -107,6 +107,10 @@ get_references <- function(results) {
 #' 
 #' @seealso \code{\link{repec_search}} for performing the initial search, and
 #'   \code{\link{get_references}} for retrieving reference data.
+#'
+#' @param results A data frame containing RIS reference data.
+#' @param file_name The path to write the output to.
+#' @export
 write_references <- function(results, file_name) {
   results$ris_data %>%
     stats::na.omit() %>%
